eb3eecff4018fc5501faa2a051900aee897ffc9d
Michael Boroski <mjboroski@gmail.com>
2018-05-25 04:12:52 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180525033104) do
+ActiveRecord::Schema.define(version: 1) do
+
+  create_table "movies", force: :cascade do |t|
+    t.string "title"
+    t.datetime "release_date"
+    t.string "director"
+    t.string "lead"
+    t.boolean "in_theaters"
+  end
 
 end
